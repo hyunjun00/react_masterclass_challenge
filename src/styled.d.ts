f5b3611eb8cea875df88592ef13b1bf2ea6f339e
hyunjun <parkhyunjun00@gmail.com>
2024-06-21 13:13:10 +0900

    Basic Animations

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,10 +2,5 @@ import "styled-components";
 
 // and extend them!
 declare module "styled-components" {
-  export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-    accentColor: string;
-    cardBgColor: string;
-  }
+  export interface DefaultTheme {}
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-  cardBgColor: "black",
-};
+export const darkTheme: DefaultTheme = {};
