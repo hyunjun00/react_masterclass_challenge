64e9ff942b75580cc60a8420fa4f7f2cf8db7152
hyunjun <parkhyunjun00@gmail.com>
2024-06-21 21:48:10 +0900

    MotionValues part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 const Wrapper = styled.div`
@@ -20,13 +20,11 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  useEffect(() => {
-    x.on("change", () => console.log(x.get()));
-  }, [x]);
+  const scale = useTransform(x, [-400, 0, 400], [2, 1, 0.1]);
   return (
     <>
       <Wrapper>
-        <Box style={{ x }} drag="x" dragSnapToOrigin />
+        <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
       </Wrapper>
     </>
   );
