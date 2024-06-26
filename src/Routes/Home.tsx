ff983bef13ae5a36dc261df3ddcfd5afca734fe4
hyunjun <parkhyunjun00@gmail.com>
2024-06-26 14:56:41 +0900

    Box Animations part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -68,6 +68,19 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const offset = 6;
 
 const boxVariants = {
@@ -79,7 +92,18 @@ const boxVariants = {
     y: -50,
     transition: {
       delay: 0.5,
-      duration: 0.3,
+      duration: 0.1,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.1,
       type: "tween",
     },
   },
@@ -137,7 +161,11 @@ function Home() {
                         initial="normal"
                         transition={{ type: "twwen" }}
                         bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                      />
+                      >
+                        <Info variants={infoVariants}>
+                          <h4>{movie.title}</h4>
+                        </Info>
+                      </Box>
                     ))}
                 </Row>
               </AnimatePresence>
